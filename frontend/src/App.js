048ebec1f876a3bfb1501db91603bca9d4fa4b5e
Guilherme Jesuino Ramires <guilhermejramires@gmail.com>
2022-10-20 00:52:23 -0300

    Lesson 7: End of lesson

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Container } from 'react-bootstrap'
 
 import Header from './components/Headers'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
 
       <main className="py-3">
         <Container>
-          <h1>Loja Profiça</h1>
+          <HomeScreen/>
         </Container>
       </main>
 
