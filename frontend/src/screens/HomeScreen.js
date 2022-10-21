ad72dbc9b54deac06bef0f2c5ac4d2b74febcfa9
Guilherme Jesuino Ramires <guilhermejramires@gmail.com>
2022-10-21 01:44:05 -0300

    Little Fix

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -7,7 +7,6 @@ import products from '../products';
 function HomeScreen() {
   return (
     <div>
-      <h1>Aqui vão os produtos</h1>
       <Row>
         {products.map(product => (
             <Col id={product._id} sm={12} md={6} lg={4} xl={3}>
