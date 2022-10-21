f67883b8b7c52b59b37557220c96dd10fe329aaf
Guilherme Jesuino Ramires <guilhermejramires@gmail.com>
2022-10-21 01:40:07 -0300

    Lesson 8,9,10,11: Improves header with routers

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
 
-function Headers() {
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from "react-router-dom"
+
+function Header() {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
-            <Navbar.Brand href="/">Loja Profiça</Navbar.Brand>
+
+            <Navbar.Brand as={Link} to="/">Loja Profiça</Navbar.Brand>
+
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="mr-auto">
-                <Nav.Link href="#home"><i className="fas fa-shopping-cart"></i> Carrinho</Nav.Link>
-                <Nav.Link href="#link"><i className="fas fa-user"></i> Login</Nav.Link>
+
+                <Nav.Link as={Link} to="/cart"><i className="fas fa-shopping-cart"></i> Carrinho</Nav.Link>
+
+                <Nav.Link as={Link} to="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+
               </Nav>
             </Navbar.Collapse>
           </Container>
@@ -20,4 +27,4 @@ function Headers() {
   )
 }
 
-export default Headers
+export default Header
