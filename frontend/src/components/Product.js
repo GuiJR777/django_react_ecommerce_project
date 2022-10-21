449f595b4c1560f83d9fbef2602f5c7c9d125e3d
Guilherme Jesuino Ramires <guilhermejramires@gmail.com>
2022-10-21 01:41:00 -0300

    Lesson 8,9,10,11: Improves Product Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,40 +1,34 @@
 import React from 'react'
+import Rating from './Rating'
+
 import { Card } from 'react-bootstrap'
+import { Link } from "react-router-dom"
+
 
 function Product({ product }) {
-    const rating_word_singular = "avaliação"
-    const rating_word_plural = "avaliações"
-
-    var rating_word = ""
-    if (product.numReviews > 1){
-        rating_word=rating_word_plural
-    }else{
-        rating_word=rating_word_singular
-    }
-
     return (
         <Card className="my-3 p-3 rounded">
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} />
-            </a>
+            </Link>
 
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title>
                         <strong>
                             {product.name}
                         </strong>
                     </Card.Title>
-                </a>
+                </Link>
 
                 <Card.Text as='div' >
                     <div className='my-3' >
-                        {product.rating} estrelas de {product.numReviews} {rating_word}
+                        <Rating value={product.rating} number_of_reviews={product.numReviews} color={'#f8e825'}/>
                     </div>
                 </Card.Text>
 
                 <Card.Text as="h3" >
-                    R${product.price}
+                    R$ {product.price}
                 </Card.Text>
             </Card.Body>
         </Card>
