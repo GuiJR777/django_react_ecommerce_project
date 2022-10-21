e74c0aa80aa079acadea4e756c5a5abe3c83748f
Guilherme Jesuino Ramires <guilhermejramires@gmail.com>
2022-10-21 18:22:30 -0300

    Lesson 12: Create routes to fake data with DRF

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,6 +1,22 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .products import products
 
 
+@api_view(["GET"])
 def get_routes(request):
-    return JsonResponse("Hello", safe=False)
+    return Response("hello")
+
+@api_view(["GET"])
+def get_products(request):
+    return Response(products)
+
+@api_view(["GET"])
+def get_product(request, primary_key):
+    return Response(__get_product_by_id(primary_key))
+
+def __get_product_by_id(primary_key: str) -> dict:
+    for product in products:
+        if product.get("_id") == primary_key:
+            return product
